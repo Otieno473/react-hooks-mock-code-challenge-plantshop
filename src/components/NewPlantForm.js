8d215bead13f993b07dba9596308c39e9bf727d0
davidotieno <davidotieno473@gmail.com>
2023-11-12 17:41:44 +0300

    complete assignment

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({addPlant}) {
+  const[fd, sfd]=useState({
+    name:"",
+    image:"",
+    price:0,
+  })
+  function onSubmit (e){
+    e.preventDefault()
+    fetch(" http://localhost:6001/plants",{
+      method: "POST",
+      headers:{
+        "content-type":"application/json"
+      },
+      body:JSON.stringify(fd)
+    })
+    .then(r=>r.json())
+    .then((np)=> {
+      addPlant(np)
+      console.log(fd)
+      sfd({name :"",
+    image :"",
+  price :0, 
+ })
+ })
+}
+function handleChange (e){
+  sfd({...fd,[e.target.name] : e.target.value})
+}
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const[pd,spd]=useState([])
+  useEffect(()=>{
+    fetch("http://localhost:6001/plants")
+    .then(r=>r.json())
+    .then((p)=>spd(p))
+  },[])
+  function addPlant (newPlant){
+    spd([...pd, newPlant])
+  }
   return (
     <main>
-      <NewPlantForm />
+      <NewPlantForm addPlant={addPlant}/>
       <Search />
-      <PlantList />
+      <PlantList ps={pd}/>
     </main>
   );
 }
